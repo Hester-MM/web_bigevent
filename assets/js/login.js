8aae52a8a3e21f290f950a22fe2164071a1fca7a
Hester <1595455407@qq.com>
2022-09-12 11:26:59 +0800

    实现了登陆与注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,67 @@
+$(function () {
+    // 点击切换登录和注册界面
+    $('#link-login').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    })
+    $('#link-reg').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+
+    // 用layui实现自定义输入框内容规则
+    // 1.获取layui里面的form对象
+    var form = layui.form;
+    form.verify({
+        //2.自定义一个pwd的密码效验规则
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        //3.校验两次密码一致的规则
+        repwd: function (value, item) {
+            //value：表单的值、item：表单的DOM对象
+            var pwd = $('.reg-box [name=password]').val();
+            if (value != pwd) {
+                return '用户输入的两次密码不一致';
+            }
+        }
+    })
+
+    //把用户注册的信息通过ajax请求发送到服务器
+    var layer = layui.layer;
+    $('#reg-form').on('submit', function (e) {
+        e.preventDefault();
+        var data = {
+            username: $('#reg-form [name=username]').val(),
+            password: $('#reg-form [name=password]').val()
+        }
+        $.post('http://big-event-api-t.itheima.net', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg('注册成功！');
+            $('#link-reg').click();
+        })
+    })
+    //监听用户登录事件
+    $('#login-form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            methods: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg('登录成功！');
+                //先获取后台权限将token字符串保存的用户信息存在localStorage中
+                localStorage.setItem('token',res.token);
+                location.href = '/index.html';
+            }
+        })
+
+    })
+
+})
